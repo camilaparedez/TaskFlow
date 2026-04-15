34056a53109a842eed32cfa6dcaec5c28a0b408a
Tomas Carignano <tomascarignano@mi.unc.edu.ar>
2026-04-15 00:27:22 -0300

    feat: integrar flujo de tareas en el menu principal

--- src/TaskFlow/Program.cs
+++ src/TaskFlow/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ejemplo de Opción 1
+case "1":
+    Console.Clear();
+    Console.WriteLine("--- Crear Nueva Tarea ---");
+    
+    Console.Write("Título: ");
+    string titulo = Console.ReadLine();
+    
+    Console.Write("Descripción: ");
+    string descripcion = Console.ReadLine();
+    
+    Console.Write("Responsable: ");
+    string responsable = Console.ReadLine();
+
+    // Llamamos al servicio para crearla
+    taskService.CrearTarea(titulo, descripcion, responsable);
+    
+    Console.WriteLine("\nPresione cualquier tecla para volver al menú...");
+    Console.ReadKey();
+    break;
