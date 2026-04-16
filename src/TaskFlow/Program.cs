cd538a49898c907f7ff11aff50aeb863bd9bad79
AgustinPelosi <agustinpelosi@icloud.com>
2026-04-16 18:51:15 -0300

    implementada la vista de listar tareas en consola

--- src/TaskFlow/Program.cs
+++ src/TaskFlow/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using TaskFlow.Services;
+TaskService servicio = new TaskService();
+var listaTareas = servicio.ObtenerTareas();
+Console.WriteLine("===LISTADO DE TAREAS===");
+if (listaTareas.Count == 0)
+{
+    Console.WriteLine("No hay tareas registradas.");
+}
+else
+{
+    foreach (var tarea in listaTareas)
+    {
+        Console.WriteLine($"[{tarea.Id}] {tarea.Title} | Resp: {tarea.Responsible} | Estado: {tarea.Status}");
+    }
+}
